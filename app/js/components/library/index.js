f8c2156e75c7365eedb6fe4a5e6b3d98d2922cdd
natebynum <nathan.bynum@axiosengineering.com>
2014-08-19 11:03:54 -0400

    Added library area under nav bar

---
+++ app/js/components/library/index.js
@@ -0,0 +1,16 @@
+/** @jsx React.DOM */
+var React = require('react');
+
+var Library = React.createClass({
+
+    render: function () {
+        return (
+            <div id="search">
+            	<h3 className="applib">Application Library</h3>
+            </div>
+        );
+    }
+
+});
+
+module.exports = Library;
