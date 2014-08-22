34e80159e316abd773c5076733131ae07934a5f3
natebynum <nathan.bynum@axiosengineering.com>
2014-08-22 09:15:04 -0400

    Added functionality to remove an app from the library upon clicking disconnect from the application library section

--- app/js/components/library/index.js
+++ app/js/components/library/index.js
@@ -10,16 +10,27 @@ var Library = React.createClass({
 	clickImage: function(url){
 		window.open(url);
 	},
+	
+	disconnect: function(app){
+		var i = this.state.appArray.indexOf(app);
+		this.state.appArray.splice(i, 1);
+		this.setState({appArray: apps});
+	},
+	
+	componentWillMount : function(){
+		this.setState({appArray: apps});
+	},
 
     render: function () {
     	var click = this.clickImage;
-    	var icons = apps.map(function(app){
+    	var disconnnect = this.disconnect;
+    	var icons = this.state.appArray.map(function(app){
     		return(
 				<li key={app.img}>					
 					
 					<i id="tileIcon" className="fa fa-ellipsis-h fa-lg" data-toggle="dropdown"></i>
 					<ul className="dropdown-menu" role="menu" id= "tileIcon-dropdown">
-	                	<li><a href="#"> Disconnect</a></li>
+	                	<li><a href="#" onClick={disconnnect.bind(this, app)}> Disconnect</a></li>
 	                </ul>					
 					<img id="applib-tiles" src={app.img} onClick={click.bind(this, app.url)}/>						
 					<h5 className="ozp-lib-name">{app.name}</h5>
