3ff63e4e3d21d680b0a81678919a70e2c893ce16
Shapiro <andrew.shapiro@axiosengineering.com>
2014-08-26 12:27:28 -0400

    Fixes to remove React and JS warnings.

--- app/js/components/alerts/index.js
+++ app/js/components/alerts/index.js
@@ -60,7 +60,7 @@ var Alerts = React.createClass({
 							<tr key={alert.text+index}>
 			    				<td><img className="alert-thumbnail" src={alert.img} /> </td>
 			    				<td className="alert-text">{alert.text}<br/><i>{alert.time}</i></td>
-			    				<td className="close"><span className="remove-alert" onClick={removeAlert.bind(this, alert)}>X</span></td>
+			    				<td className="close"><span className="remove-alert" onClick={removeAlert.bind(null, alert)}>X</span></td>
 			    			</tr>
 						);
 					});
