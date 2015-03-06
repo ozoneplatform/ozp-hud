f257dda6f967f16ac24453b53ba508ec938ec059
Nish <nishantpatel611@gmail.com>
2015-03-05 19:27:04 -0500

    feat(Icons): Integrate with icons to use svgs

--- app/js/components/header/index.jsx
+++ app/js/components/header/index.jsx
@@ -45,9 +45,9 @@ var Header = React.createClass({
                 <div className="container-fluid">
                     <div className="navbar-left">
                         <ul className="nav navbar-nav">
-                            <li className="active"><a className="lrg" href={HUD_URL}><i className="icon-home"></i></a></li>
-                            <li><a className="lrg" href={CENTER_URL}><i className="icon-shopping"></i></a></li>
-                            <li><a className="lrg" href={WEBTOP_URL}><i className="icon-layout"></i></a></li>
+                            <li className="active"><a className="lrg" href={HUD_URL}><i className="icon-home-blue"></i></a></li>
+                            <li><a className="lrg" href={CENTER_URL}><i className="icon-shopping-grayLightest"></i></a></li>
+                            <li><a className="lrg" href={WEBTOP_URL}><i className="icon-layout-grayLightest"></i></a></li>
                         </ul>
                     </div>
 
