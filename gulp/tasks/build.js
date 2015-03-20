2670aa20119ab7da04ad69527ae835d20d7dcdd1
Alan Ward <arw180@gmail.com>
2015-03-20 13:01:31 -0400

    remove duplicate jQuery lib

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,3 +1,4 @@
 var gulp = require('gulp');
 
-gulp.task('build', ['sass', 'copy', 'config', 'vendor', 'fonts', 'svg', 'webpack:prod']);
+gulp.task('build', ['sass', 'copy', 'config', 'vendor', 'fonts', 'svg',
+  'jqueryUndupe', 'webpack:prod']);
