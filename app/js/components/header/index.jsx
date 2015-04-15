6ca9b3b6b1e63c18730e165fa152dd95e084b18c
Sarah Deighan <sarah.deighan@nextcentury.com>
2015-04-15 12:14:38 -0400

    fix(Feedback Link): #77 Link now includes mailto:
    Configuration now no longer requires “mailto:”, it is included in the
    link instead

--- app/js/components/header/index.jsx
+++ app/js/components/header/index.jsx
@@ -84,7 +84,7 @@ var Header = React.createClass({
                                         <li><a href={CENTER_URL + '#/mall-management/categories'}><i className="icon-shopping-settings-grayLightest"></i>Marketplace Settings</a></li>
                                     }
                                     { Metrics }
-                                    <li><a href={FEEDBACK_ADDRESS} className="caboose"><i className="icon-mail-white"></i>Submit Feedback</a></li>
+                                    <li><a href={'mailto:'+FEEDBACK_ADDRESS} className="caboose"><i className="icon-mail-white"></i>Submit Feedback</a></li>
                                 </ul>
                             </li>
                         </ul>
