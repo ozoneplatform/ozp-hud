6459ebcdcb2d0e2fd6b017b5179111976ea947c3
wski <contact.mattdylan@gmail.com>
2015-06-08 16:51:51 -0400

    feat(Docs and Videos): Added docs and videos to help modal #90

--- app/js/components/header/helpmodal.jsx
+++ app/js/components/header/helpmodal.jsx
@@ -1,8 +1,7 @@
 'use strict';
 
 var React = require('react');
-
-var { HELP_URL } = require('OzoneConfig');
+var { /*HELP_URL,*/ HELP_DOCS, HELP_VIDEOS } = require('ozp-react-commons/OzoneConfig');
 
 var HelpModal = React.createClass({
 
@@ -30,22 +29,62 @@ var HelpModal = React.createClass({
             });
     },
 
+    fetchDocs: (()=>{
+      var docArr = [];
+      for(var doc in HELP_DOCS){
+        docArr.push(HELP_DOCS[doc]);
+      }
+      return docArr;
+    })(),
+
+    fetchVids: (()=>{
+      var vidArr = [];
+      for(var vid in HELP_VIDEOS){
+        vidArr.push(HELP_VIDEOS[vid]);
+      }
+      return vidArr;
+    })(),
+
     render: function () {
+        var docLinks = this.fetchDocs.map(function(link){
+          return(
+            <li>
+              <a target="_blank" href={ link }>{ link }</a>
+            </li>
+          );
+        });
+        var vidLinks = this.fetchVids.map(function(link){
+          return(
+            <li>
+              <a target="_blank" href={ link }>{ link }</a>
+            </li>
+          );
+        });
         return (
             <div id="help-modal" className="modal fade" role="dialog" aria-hidden="true">
                 <div className="modal-dialog  modal-lg">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <button type="button" className="close" data-dismiss="modal">
-                                <span aria-hidden="true">
-                                    <i className="icon-cross-18-grayLightest"></i>
-                                </span>
-                                <span className="sr-only">Close</span>
-                            </button>
-                            <h4 className="modal-title">OZONE Help Zone</h4>
+                        <button type="button" className="close" data-dismiss="modal"><span aria-hidden="true"><i className="icon-cross-18"></i></span><span className="sr-only">Close</span></button>
+                        <h4 className="modal-title">OZONE Help Zone</h4>
                         </div>
                         <div className="modal-body">
-                            <iframe style={{width:"100%", height:"500px", border: 'none'}} src={HELP_URL} />
+                          <div style={{'overflow': 'auto'}}>
+                            <div className="col-md-6">
+                              <h4>Documents</h4>
+                              <ul>
+                                {docLinks}
+                              </ul>
+                            </div>
+
+                            <div className="col-md-6">
+                              <h4>Videos</h4>
+                              <ul>
+                                {vidLinks}
+                              </ul>
+                            </div>
+                          </div>
+                            {/*<iframe style={{width:"100%", height:"500px", border: 'none'}} src={HELP_URL} />*/}
                         </div>
                     </div>
                 </div>
