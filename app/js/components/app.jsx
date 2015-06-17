2661c40e699261e63ee1d5b3cbcf3d10bbb7fe0e
Stephanie Schneider <sschneider713@gmail.com>
2015-06-17 16:11:17 -0400

    feat(custom HUD): added support for gridster

--- app/js/components/app.jsx
+++ app/js/components/app.jsx
@@ -1,6 +1,7 @@
 'use strict';
 
 var React = require('react');
+var ReactGridLayout = require('react-grid-layout');
 var { RouteHandler } = require('react-router');
 var Header = require('./header/index.jsx');
 var Settings = require('./settings/index.jsx');
@@ -26,9 +27,17 @@ var App = React.createClass({
         return (
             <div>
                 <Header />
-                <MyListings />
-                <AllListings />
-                <Library />
+                <ReactGridLayout className="layout" cols={2} rowHeight={200} isResizeable={true}>
+                    <div key={1} _grid={{x: 0, y: 0, w: 1, h: 2}}>
+                        <MyListings />
+                    </div>
+                    <div key={2} _grid={{x: 1, y: 0, w: 1, h: 2}}>
+                        <AllListings />
+                    </div>
+                    <div key={3} _grid={{x: 0, y: 1, w: 1, h: 2}} isResizeable={true}>
+                        <Library />
+                    </div>
+                </ReactGridLayout>
                 <Settings />
                 <RouteHandler params={ this.props.params } />
             </div>
