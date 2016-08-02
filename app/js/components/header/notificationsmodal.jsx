61f2c137e7bc92952b6e08a4dd266fbd9cd46dd5
Matt Wisniewski <contact.mattdylan@gmail.com>
2016-08-02 15:00:03 -0400

    feat(Shared folders): Notification modal changes applied.

--- app/js/components/header/notificationsmodal.jsx
+++ app/js/components/header/notificationsmodal.jsx
@@ -3,6 +3,8 @@
 var React = require('react');
 var Reflux = require('reflux');
 var SelfActions = require('ozp-react-commons/actions/ProfileActions.js');
+var LibraryActions = require('../../actions/Library');
+var { API_URL } = require('ozp-react-commons/OzoneConfig');
 
 var marked = require('marked');
 var renderer = new marked.Renderer();
@@ -67,13 +69,13 @@ var NotificationsModal = React.createClass({
         var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
         var formattedDate = months[date.getMonth()] + ' ' + date.getDate() + ', ' +  date.getFullYear();
         return (
-          <li role="presentation" alt={`Notification ${i + 1} from ${n.author.user.username}`} tabIndex={i} onClick={() => {
+          <li role="presentation" alt={`Notification ${i + 1} from AppsMall`} tabIndex={i} onClick={() => {
               this.setState({
                 activeNotification: i
               });
             }}>
             <a href="#" onClick={(e) => {e.preventDefault();}}>
-              {n.author.user.username} <small>{formattedDate}</small>
+              AppsMall <small>{formattedDate}</small>
             </a>
           </li>
         );
@@ -90,24 +92,52 @@ var NotificationsModal = React.createClass({
       return (
         <div>
           <div className="row" tabIndex={0}>
-            <h4>{n.author.user.username} <small>{formattedDate}</small></h4>
-            <p>
-              <div dangerouslySetInnerHTML={createNotificationText()} />
+            <h4>{(n.listing) ? n.listing.title : 'AppsMall'} <small>{formattedDate}</small></h4>
+            <span>
+
+              { !(n.notificationType === "PEER.BOOKMARK") &&
+                <span className="message small" dangerouslySetInnerHTML={createNotificationText()}></span>
+              }
+              { n.notificationType === "PEER.BOOKMARK" &&
+                <div>
+                  <p className="message small">{n.author.user.username} has shared a the folder <b>{n.peer.folderName}</b> with you.</p>
+                  <p className="message small">{n.message}</p>
+                  <div>
+                    <button className="btn btn-default btn-sm" onClick={() => {
+                      this.onDismiss(n);
+                    }}>Ignore</button>
+                    <button className="btn btn-success btn-sm" onClick={() => {
+                        $.ajax({
+                            type: 'POST',
+                            dataType: 'json',
+                            contentType: 'application/json',
+                            url: API_URL + '/api/self/library/import_bookmarks/',
+                            data: JSON.stringify({
+                              "bookmark_notification_id": n.id
+                            })
+                        }).done(() => {
+                          this.onDismiss(n);
+                        });
+                      }}>Add {n.peer.folderName}</button>
+                  </div>
+                </div>
+              }
               <br /><br />
-              <button className="btn btn-danger right" aria-label={`Remove notification from ${n.author.user.username}`} onClick={() => {
+              <button className="btn btn-danger right" aria-label={`Remove notification from ${(n.listing) ? n.listing.title : 'AppsMall'}`} onClick={() => {
                   this.onDismiss(
                     this.state.notificationList[this.state.activeNotification]
                   );
                 }}>
                 Remove Notification
               </button>
-            </p>
+            </span>
           </div>
         </div>
       );
     },
 
     onDismiss(notification) {
+      LibraryActions.fetchLibrary();
       SelfActions.dismissNotification(notification);
     },
 
@@ -141,28 +171,10 @@ var NotificationsModal = React.createClass({
                                 }
 
                                 { !this.state.notificationList.length &&
-                                  <span>Loading...</span>
+                                  <span>No notifications</span>
                                 }
                               </div>
                             </div>
-                            {/*
-                            <div className="row">
-                              <div className="col-xs-12 form-inline">
-                                <fieldset className="form-group">
-                                  <label for="exampleSelect1">Sort By: &nbsp;</label>
-                                  <select className="form-control" id="exampleSelect1">
-                                    <option>Date</option>
-                                  </select>
-                                  &nbsp;&nbsp;
-                                  <label for="exampleSelect1">From Sender: &nbsp;</label>
-                                  <select className="form-control" id="exampleSelect1">
-                                    <option>Apps Mall</option>
-                                  </select>
-                                </fieldset>
-                              </div>
-                            </div>
-                            */}
-
                         </div>
                     </div>
                 </div>
