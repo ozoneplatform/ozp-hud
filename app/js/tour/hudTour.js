37915b6af96bc18d9fc0eae78bc058bce063fac3
rkenyon1969 <rjkenyon@gmail.com>
2016-08-30 10:07:51 -0400

    update

--- app/js/tour/hudTour.js
+++ app/js/tour/hudTour.js
@@ -203,7 +203,7 @@ const meTour = new Tour({
       backdropPadding: 0,
       template: '<div class="popover" role="tooltip"> <div class="arrow"></div> <h3 class="popover-title"></h3> <div class="popover-content"></div> <div class="popover-navigation"> <button class="btn btn-sm" id="end-tour-btn" data-role="end">End tour</button> <div class="btn-group"> <button class="btn btn-sm btn-default" data-role="prev">&laquo; Prev</button> ' + contentLocalCenter + '<button class="btn btn-sm btn-default" data-role="pause-resume" data-pause-text="Pause" data-resume-text="Resume">Pause</button> </div> </div> </div>',
       orphan: true,
-      onShown: function() {
+      onShow: function() {
         tourDBMain.set({
           hud: {
             ran: true,
