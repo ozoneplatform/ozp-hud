75237a1f984e6cf6ed8efa071eedf74cba8a1b69
Manny Rivera <mannyrivera2010@gmail.com>
2018-01-12 13:19:15 -0500

    release(script): fix release script

--- jenkins/release.sh
+++ jenkins/release.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 source /usr/local/node_versions/set_node_version.sh 5.3.0
-npm install bower grunt-cli
 npm install
-npm run bower
+npm run install
 npm run build
-npm run compile
-npm run tarDevVersion
-npm run tarProdVersion
+npm run tarDistVersion
